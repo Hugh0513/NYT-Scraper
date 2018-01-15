f13139246c35a2fadac661470456f101bd4d9d57
Ryosaku <hiwaryo@gmail.com>
2018-01-14 23:50:16 -0800

    better

--- server.js
+++ server.js
@@ -43,6 +43,8 @@ mongoose.connect("mongodb://localhost/hugh-scraper", {
 
 // heroku
 mongoose.connect("mongodb://heroku_dqs126r3:gtghhunrqqg00evv1f79jlq80l@ds245277.mlab.com:45277/heroku_dqs126r3",{
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE,
   useMongoClient: true
 });
 
